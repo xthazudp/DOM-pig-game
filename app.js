f63232008dd5158da80d094f73e670e7e8be2039
xthazudp <xthazudp@gmail.com>
2020-08-03 11:32:41 +0545

    toggle active player

--- app.js
+++ app.js
@@ -31,6 +31,27 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the roundScore untill the rolled number is not 1
+
+    if (dice !== 1) {
+        // addScore
+        roundScore+= dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // nextPlayer
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+    }
 });
 
 
