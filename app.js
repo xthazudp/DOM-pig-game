62b3ab4faa4e67a27db630cc7a9eaff5d36c693a
xthazudp <xthazudp@gmail.com>
2020-08-03 09:37:08 +0545

    commit

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores=[0, 0];
+roundScore= 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6 )+ 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
