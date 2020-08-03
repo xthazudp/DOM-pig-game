aeaddbb5e3953e127cd8e41e04195039a1a7ab80
xthazudp <xthazudp@gmail.com>
2020-08-03 12:07:38 +0545

    nextPlayer toggle

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ GAME RULES:
 var scores, roundScore, activePlayer;
 
 scores=[0, 0];
-roundScore= 0;
+roundScore = 0;
 activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
@@ -36,23 +36,11 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
     if (dice !== 1) {
         // addScore
-        roundScore+= dice;
+        roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // nextPlayer
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-0-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
+        nextPlayer();
     }
 });
 
@@ -62,9 +50,26 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
     // update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // nextPlayer
+    nextPlayer();
 });
 
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+}
 
 
 
